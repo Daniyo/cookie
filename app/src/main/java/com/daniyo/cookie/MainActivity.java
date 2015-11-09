ec271dfaac61fb09178eefd6a4429bd86e5a483a
Daniel Breckel <daniel.breckel@uni-ulm.de>
2015-11-09 21:18:23 +0100

    added button and image; updated jdk version
    no idea why the jdk version changed

--- app/src/main/java/com/daniyo/cookie/MainActivity.java
+++ app/src/main/java/com/daniyo/cookie/MainActivity.java
@@ -1,7 +1,10 @@
 package com.daniyo.cookie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hasteButtonPressed(View view){
+        ImageView monkeyImage = (ImageView) findViewById(R.id.imageView);
+        int visible = monkeyImage.getVisibility();
+        if(visible == View.VISIBLE)
+            monkeyImage.setVisibility(View.INVISIBLE);
+        else
+            monkeyImage.setVisibility(View.VISIBLE);
     }
 }
